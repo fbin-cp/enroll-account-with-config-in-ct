8331b54b94b3a3fcd99e8bf68054321db6d1d24f
Sebastien Dejean <sebastien.dejean@fbwinn.com>
2025-06-24 12:14:59 -0700

    Fix syntax error on invalid escape sequence

--- fetch-account-details.py
+++ fetch-account-details.py
@@ -167,4 +167,4 @@ if __name__ == '__main__':
         print('\n'.join('{}: {}'.format(*k) for k in enumerate(accounts, start=1)))
         LOGGER.info('------------')
     else:
-        error_and_exit('\n/!\ ERROR : Supplied OU does not exist.\n')
+        error_and_exit('\n/!ERROR : Supplied OU does not exist.\n')
